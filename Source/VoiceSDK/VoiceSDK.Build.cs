aebe565eba3ba34a22b3ab07d9fca2bce606b304
王涛tom <tom@sinceme.com>
2017-04-07 18:56:16 +0800

    fixed bug

--- Source/VoiceSDK/VoiceSDK.Build.cs
+++ Source/VoiceSDK/VoiceSDK.Build.cs
@@ -6,7 +6,7 @@ public class VoiceSDK : ModuleRules
 {
 	public VoiceSDK(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "FlytekVoiceSDK" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
