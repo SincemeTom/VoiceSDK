3706c4a456ae206116a115e3aee5d4f54df1f3ba
王涛tom <tom@sinceme.com>
2017-04-07 14:05:46 +0800

    Add Login Function

--- Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/FlytekVoiceSDK.cpp
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/FlytekVoiceSDK.cpp
@@ -1,22 +1,62 @@
 // Copyright 2016 Gamemakin LLC. All Rights Reserved.
 
 
-
-
 #include "FlytekVoiceSDK.h"
-
+#include "Paths.h"
+#include "qisr.h"
 
 #define LOCTEXT_NAMESPACE "FFlytekVoiceSDKModule"
 
+
+FFlytekVoiceSDKModule::FFlytekVoiceSDKModule()
+{
+
+}
+FFlytekVoiceSDKModule::~FFlytekVoiceSDKModule()
+{
+
+}
 void FFlytekVoiceSDKModule::StartupModule()
 {
-	
+	if (IsAvailable())
+	{
+		UE_LOG(LogFlytekVoiceSDK, Log, TEXT("%s"), TEXT("Module Started"));
+	}
+
+	// Get the base directory of this plugin
+	FString BaseDir = IPluginManager::Get().FindPlugin("FlytekVoiceSDK")->GetBaseDir();
+
+	// Add on the relative location of the third party dll and load it
+	FString LibraryPath;
+#if PLATFORM_WINDOWS
+#if PLATFORM_64BITS
+	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/VR7663SDK/Win64/VR7663Dll.dll"));
+#else
+	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/VR7663SDK/Win32/VR7663Dll.dll"));
+#endif
+#endif
+
+	VR7663DllHandle = !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
+
+	if (VR7663DllHandle)
+	{
+		// Call the test function in the third party library that opens a message box
+	}
+	else
+	{
+		FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("ThirdPartyLibraryError", "Failed to load vr platform sdk third party library"));
+	}
 
 }
 
 void FFlytekVoiceSDKModule::ShutdownModule()
 {
 	
+}
+
+int32 FFlytekVoiceSDKModule::VoiceSDKLogin(const FString& UserName, const FString& Password, const FString& Params)
+{
+	return 0;
 }
 
 
--- Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Public/FlytekVoiceSDK.h
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Public/FlytekVoiceSDK.h
@@ -2,16 +2,57 @@
 
 #pragma once
 
-#include "Modules/ModuleManager.h"
+#include "ModuleManager.h"
+#include "ModuleInterface.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogFlytekVoiceSDK, Verbose, All);
 
-class FFlytekVoiceSDKModule : public IModuleInterface
+
+
+
+class IFlytekVoiceSDK : public IModuleInterface
 {
+
 public:
 
+	static FName GetModularFeatureName()
+	{
+		static FName FeatureName = FName(TEXT("FlytekVoiceSDK"));
+		return FeatureName;
+	}
+
+	/**
+	* Singleton-like access to this module's interface.  This is just for convenience!
+	* Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	*
+	* @return Returns singleton instance, loading the module on demand if needed
+	*/
+	static inline IFlytekVoiceSDK& Get()
+	{
+		return FModuleManager::LoadModuleChecked< IFlytekVoiceSDK >("FlytekVoiceSDK");
+	}
+
+	/**
+	* Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	*
+	* @return True if the module is loaded and ready to use
+	*/
+	static inline bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded("FlytekVoiceSDK");
+	}
+	//int MSPAPI MSPLogin(const char* usr, const char* pwd, const char* params);
+	virtual int32 VoiceSDKLogin(const FString& UserName, const FString& Password, const FString& Params) = 0;
+};
+class FFlytekVoiceSDKModule : public IFlytekVoiceSDK
+{
+public:
+	FFlytekVoiceSDKModule();
+	~FFlytekVoiceSDKModule();
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+	virtual int32 VoiceSDKLogin(const FString& UserName, const FString& Password, const FString& Params) override;
 
 };
