d9b187e88a1e0e671938e82a2dc692aa117a342c
王涛tom <tom@sinceme.com>
2017-04-11 19:31:05 +0800

    Add treadClass

---
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/TreadClass.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TreadClass.h"
+#include "FlytekVoiceSDK.h"
+
+
+TreadClass::TreadClass()
+{
+}
+
+TreadClass::~TreadClass()
+{
+}
+uint32 TreadClass::Run()
+{
+	return 0;
+}
+
+void TreadClass::Stop()
+{
+
+}
+
+void TreadClass::Exit()
+{
+
+}
