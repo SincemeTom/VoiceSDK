95983319ad4bf6c5caebc97b03de6e0c9595203f
王涛tom <tom@sinceme.com>
2017-06-23 16:57:17 +0800

    fix build error

--- Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/FlytekVoiceSDK.Build.cs
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/FlytekVoiceSDK.Build.cs
@@ -4,65 +4,56 @@ using UnrealBuildTool;
 
 public class FlytekVoiceSDK : ModuleRules
 {
-	public FlytekVoiceSDK(TargetInfo Target)
-	{
+    public FlytekVoiceSDK(TargetInfo Target)
+    {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicIncludePaths.AddRange(
-			new string[] {
+            new string[] {
                 "FlytekVoiceSDK/Public"
 				// ... add public include paths required here ...
 			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
                 "FlytekVoiceSDK/Private",
 				// ... add other private include paths required here ...
 			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
                 "IFlytekSDK"
 				// ... add other public dependencies that you statically link with here ...
 			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
             {
                 "CoreUObject",
                 "Engine",
                 "Slate",
                 "SlateCore",
-                "UnrealEd",
                 "UMG",
-                "UMGEditor",
-                "ContentBrowser",
-                "AssetTools",
-                "EditorStyle",
                 "Projects",
-                "MediaAssets",
-                "Landscape",
-                "AIModule",
                 "NetworkReplayStreaming",
-                "BlueprintGraph"
 				// ... add private dependencies that you statically link with here ...	
 			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
+            );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
                 //"LevelSequence",
             }
-			);
-	}
+            );
+    }
 }
