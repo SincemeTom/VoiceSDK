3a91e6725a754fa788fce26d5d2964197e6b3e01
王涛tom <tom@sinceme.com>
2017-06-29 10:38:44 +0800

    update 4.14

--- Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/FlytekVoiceSDK.cpp
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/FlytekVoiceSDK.cpp
@@ -1,6 +1,6 @@
 // Copyright 2016 Gamemakin LLC. All Rights Reserved.
 
-
+#include "FlytekVoiceSDKPrivatePCH.h"
 #include "FlytekVoiceSDK.h"
 #include "Paths.h"
 #include "IPluginManager.h"
--- Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/SpeechRecognizeTask.cpp
+++ Plugins/FlytekVoiceSDK/Source/FlytekVoiceSDK/Private/SpeechRecognizeTask.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "FlytekVoiceSDKPrivatePCH.h"
 #include "SpeechRecognizeTask.h"
 
 template <typename T>
